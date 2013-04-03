b548cd8060138d3d524eb598af5cf8090b612461
sbustamante <macsebas33@gmail.com>
2013-04-03 12:09:22 -0500

    Removing empty files

--- plynet/conf/thermalrc.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-#oooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooo
-#	CONFIGURATION FILE OF THERMAL MODULE
-#oooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooo
-signature='thermal'
-
